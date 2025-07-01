55c6c3eaa6455c25579c05f732e466ca222e801b
solderfumes <luka.schuller@gmail.com>
2025-06-30 21:06:49 -0700

    Fixed Darkmode preferences

--- darkmode.js
+++ darkmode.js
@@ -4,16 +4,18 @@ themeButton.onclick = toggleTheme;
 let darkModeIcon = 'https://img.icons8.com/?size=40&id=45475&format=png&color=000000'
 let lightModeIcon = 'https://img.icons8.com/?size=40&id=45475&format=png&color=ffffff'
 
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
-    if (event.matches) {
-        document.body.setAttribute('data-theme', 'dark');
-        themeButton.src = lightModeIcon;
-    }
-    else {
-        document.body.setAttribute('data-theme', light);
-        themeButton.src = darkModeIcon;
-    }
-});
+prefersLightTheme = window.matchMedia('(prefers-color-scheme: light)');
+//initialize theme
+if (prefersLightTheme.matches) {
+    console.log('prefers lightmode');
+    document.body.setAttribute('data-theme', 'light');
+    themeButton.src = darkModeIcon;
+}
+else {
+    console.log('prefers darkmode');
+    document.body.setAttribute('data-theme', 'dark');
+    themeButton.src = lightModeIcon;
+}
 
 function toggleTheme() {
     if (document.body.getAttribute('data-theme') == 'dark') {
